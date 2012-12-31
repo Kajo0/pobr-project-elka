169c81878bcaed88d483545ef496f3fffcd243ab
kajo100@gmail.com <kajo100@gmail.com@ae1b9bf0-70db-9db8-c90e-8c7fb5c75078>
2012-12-31 20:21:30 +0000

    boxy parts recognizing but not combined with amd

--- trunk/src/pl/edu/pw/elka/mmarkiew/model/SegmentType.java
+++ trunk/src/pl/edu/pw/elka/mmarkiew/model/SegmentType.java
@@ -7,6 +7,6 @@ package pl.edu.pw.elka.mmarkiew.model;
  */
 public enum SegmentType {
 
-	LETTER_A, LETTER_M, LETTER_D, NEXT_TO_LETTERS_BOXY, UNDEFINED;
+	LETTER_A, LETTER_M, LETTER_D, BOTTOM_LEFT_BOXY, TOP_RIGHT_BOXY, UNDEFINED;
 
 }
